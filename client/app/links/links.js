c21b73fbe0f34d55609ae8ac68a3e87ee3851326
whatrocks <charles.harrington@gmail.com>
2015-11-06 15:24:43 -0800

    Revert "Passing tests for links. Removed git pre commit per instructions."
    This reverts commit 8a9c3d2ff363173a1a7392cc0d5b32e094fd9add.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,20 +2,4 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
-  $scope.data= {};
-
-  $scope.getLinks = function() {
-
-    Links.getLinks()
-      .then(function(links) {
-        $scope.data.links = links;
-      })
-      .catch(function(error){
-        console.error(error);
-      });
-
-  };
-
-  $scope.getLinks();
-
 });
